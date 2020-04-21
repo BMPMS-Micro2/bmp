012d4ff9c50b5c540b6436b4b153eecb983b5df2
EnriqueJavierG <enrique.gonzalez6@upr.edu>
2020-04-21 16:14:50 -0400

    Initial Experiment Page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css'
+import Experiment from "./components/experiment";
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Experiment />, document.getElementById('root'))
